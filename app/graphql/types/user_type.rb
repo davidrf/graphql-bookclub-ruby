bec33d6269ee70741e970e27932b346d9bafefc3
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2018-11-26 02:00:08 -0500

    exercise 5 solved

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,6 +1,7 @@
 class Types::UserType < Types::BaseObject
   field :bio, String, null: true
   field :first_name, String, null: false
+  field :full_name, String, null: false
   field :id, ID, null: false
   field :last_name, String, null: false
   field :picture_url, String, null: true
