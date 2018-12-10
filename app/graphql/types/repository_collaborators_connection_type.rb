9ab3cd32037df7f366c1f050cd2e2e6be9ad5057
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2018-12-10 11:54:13 -0500

    meeting 4 exercise 5 solved

--- app/graphql/types/repository_collaborators_connection_type.rb
+++ app/graphql/types/repository_collaborators_connection_type.rb
@@ -1,6 +1,11 @@
 class Types::RepositoryCollaboratorsConnectionType < GraphQL::Types::Relay::BaseConnection
   edge_type(Types::RepositoryCollaboratorsEdgeType, edge_class: Edges::RepositoryCollaboratorsEdge)
 
+  field :collaborators, [Types::UserType], null: false
+  def collaborators
+    object.edge_nodes
+  end
+
   field :total_count, Integer, null: false
   def total_count
     object.nodes.size
